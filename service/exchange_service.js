d33ecb68daa437b507f33a714906c6a748bb4b8c
devni-heraliyawala <devniheraliyawala314@gmail.com>
2022-01-29 15:22:07 +0530

    Testing rollback reject on prod pipeline demo

--- service/exchange_service.js
+++ service/exchange_service.js
@@ -22,7 +22,7 @@ mysql_connection.connect((err) => {
 var exchange_service = {
   info: (req, res, next) => {
     client.rest.account.listAccounts().then((accounts) => {
-      const message = `You can trade new (coinbase) "${accounts.length}" different pairs. blue-green rollback testing 1.8`;
+      const message = `You can trade new (coinbase) "${accounts.length}" different pairs. blue-green rollback testing 1.9`;
       console.log(message);
       res.send(message);
     });
