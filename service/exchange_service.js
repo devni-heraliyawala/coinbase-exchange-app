d8bd5d8434f8102666ca6f1b809225d99e02704b
devni-heraliyawala <devniheraliyawala314@gmail.com>
2022-01-29 14:47:26 +0530

    testing prod pipeline

--- service/exchange_service.js
+++ service/exchange_service.js
@@ -22,7 +22,7 @@ mysql_connection.connect((err) => {
 var exchange_service = {
   info: (req, res, next) => {
     client.rest.account.listAccounts().then((accounts) => {
-      const message = `You can trade new (coinbase) "${accounts.length}" different pairs. blue-green rollback testing 1.5`;
+      const message = `You can trade new (coinbase) "${accounts.length}" different pairs. blue-green rollback testing 1.6`;
       console.log(message);
       res.send(message);
     });
